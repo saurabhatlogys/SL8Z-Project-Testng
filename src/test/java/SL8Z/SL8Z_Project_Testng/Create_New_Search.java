4dfb9928935ef1698748a3d630e8509ae8fdc4f4
saurabhatlogys <saurabh@atlogys.com>
2014-01-23 14:40:40 +0530

    Fifth Commit

--- src/test/java/SL8Z/SL8Z_Project_Testng/Create_New_Search.java
+++ src/test/java/SL8Z/SL8Z_Project_Testng/Create_New_Search.java
@@ -49,7 +49,7 @@ public class Create_New_Search {
 	  driver.findElement(By.xpath("//a[@id='ui-id-2']")).click();
 	  Reporter.log("Clicked on 2nd Tab");
 	  // Position Title
-	  driver.findElement(By.xpath("//input[@id='title']")).sendKeys("Test Automated Post 7");
+	  driver.findElement(By.xpath("//input[@id='title']")).sendKeys("Test Automated Post 9");
 	  Reporter.log("Entered Position Title");
 	  // Position Metro Area 
 	  //driver.findElement(By.xpath("//input[@id='location_metro_area']")).sendKeys("ban");
